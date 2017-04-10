e400245d450afd6875c61dfbd66d2c524db89f7e
Dombovari Peter <dombo.peter@gmail.com>
2017-04-10 12:34:09 +0200

    style: remove comments

--- box/Box.java
+++ box/Box.java
@@ -5,8 +5,4 @@ public class Box {
     System.out.println(firstCuboid.getSurface());
     System.out.println(firstCuboid.getVolume());
   }
-  // Create a class that represents a cuboid:
-  // It should take its three dimensions as constructor parameters (numbers)
-  // It should have a method called `getSurface` that returns the cuboid's surface
-  // It should have a method called `getVolume` that returns the cuboid's volume
 }
