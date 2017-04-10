3ceb905cc574c49781d43347cd823b19daaa719f
Dombovari Peter <dombo.peter@gmail.com>
2017-04-10 10:10:06 +0200

    refact: import necessary packages

--- seconds/Seconds.java
+++ seconds/Seconds.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Seconds {
   public static void main(String[] args) {
     // Create a function that takes a list as a parameter,
