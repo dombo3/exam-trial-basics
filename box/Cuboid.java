65c7a88afad92ffb89bfcec1132f2fc04b3250cd
Dombovari Peter <dombo.peter@gmail.com>
2017-04-10 12:24:00 +0200

    feat(Cuboid): create Cuboid skeleton with fields

---
+++ box/Cuboid.java
@@ -0,0 +1,18 @@
+public class Cuboid {
+  private int x;
+  private int y;
+  private int z;
+
+  public Cuboid(int x, int y, int z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  public int getSurface() {
+  }
+
+  public int getVolume() {
+
+  }
+}
