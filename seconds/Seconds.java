bae64d9d58bd1ce403ca33243bf9c4c4703e3384
Dombovari Peter <dombo.peter@gmail.com>
2017-04-10 10:27:46 +0200

    refact: refact code in numbersList method, return was in wrong place

--- seconds/Seconds.java
+++ seconds/Seconds.java
@@ -7,16 +7,17 @@ public class Seconds {
     // Create a function that takes a list as a parameter,
     // and returns a new list with every second element from the orignal list
     // example: [1, 2, 3, 4, 5] should produce [2, 4] - print this result
-    List<Integer> myList = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+    List<Integer> myList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
     System.out.println(numbersList(myList));
   }
 
-  public List<Integer> numbersList(List<Integer> numbers) {
+  public static List<Integer> numbersList(List<Integer> numbers) {
     List<Integer> result = new ArrayList();
     for (int i = 0; i < numbers.size(); i++) {
       if (!(i % 2 == 0)) {
         result.add(numbers.get(i));
       }
+    }
     return result;
   }
 }
