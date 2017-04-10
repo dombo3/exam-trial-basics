43bdc52476de34ef1f16ae8071d67bf0fa207766
Dombovari Peter <dombo.peter@gmail.com>
2017-04-10 12:17:10 +0200

    style: remove comments

--- pirates/Pirates.java
+++ pirates/Pirates.java
@@ -29,18 +29,12 @@ public class Pirates{
   public static void main(String... args){
     ArrayList<Pirate> pirates = new ArrayList<>();
 
-    // Given this list...
-
     pirates.add(new Pirate("Olaf", false, 12));
     pirates.add(new Pirate("Uwe", true, 9));
     pirates.add(new Pirate("Jack", true, 16));
     pirates.add(new Pirate("Morgan", false, 17));
     pirates.add(new Pirate("Hook", true, 20));
 
-    // Write a function that takes any list that contains pirates as in the example,
-    // And returns a list of names containing the pirates that
-    // - have wooden leg and
-    // - have more than 15 gold
     System.out.println(getWoodenLegMoreThan15goldPirates(pirates));
 
   }
