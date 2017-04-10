335310e079a3b8857368e76e44ecbe7e9afb7c15
Dombovari Peter <dombo.peter@gmail.com>
2017-04-10 11:41:34 +0200

    refact: refact given code, missing brackets, missing class modifier, rename class, and why is main inside Pirates...

--- pirates/Pirates.java
+++ pirates/Pirates.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
-public class PirateCounter{
+public class Pirates{
 
-  class Pirate {
+  static class Pirate {
     String name;
     boolean hasWoodenLeg;
     int gold;
@@ -19,11 +19,11 @@ public class PirateCounter{
 
     // Given this list...
 
-    pirates.add(new Pirate("Olaf", false, 12);
-    pirates.add(new Pirate("Uwe", true, 9);
-    pirates.add(new Pirate("Jack", true, 16);
-    pirates.add(new Pirate("Morgan", false, 17);
-    pirates.add(new Pirate("Hook", true, 20);
+    pirates.add(new Pirate("Olaf", false, 12));
+    pirates.add(new Pirate("Uwe", true, 9));
+    pirates.add(new Pirate("Jack", true, 16));
+    pirates.add(new Pirate("Morgan", false, 17));
+    pirates.add(new Pirate("Hook", true, 20));
 
     // Write a function that takes any list that contains pirates as in the example,
     // And returns a list of names containing the pirates that
