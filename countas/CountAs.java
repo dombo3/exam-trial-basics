30bb283ced22a2c7ec58ce9b5a1855c848b87641
Dombovari Peter <dombo.peter@gmail.com>
2017-04-10 12:18:23 +0200

    style: remove comments

--- countas/CountAs.java
+++ countas/CountAs.java
@@ -6,12 +6,7 @@ import java.util.List;
 
 public class CountAs {
   public static void main(String[] args) {
-    // Create a function that takes a filename as string parameter,
-    // counts the occurances of the letter "a", and returns it as a number.
-    // If the file does not exist, the function should return 0 and not break.
-
-    // example: on the input "afile.txt" the function should return 28 - print this result
-    // example: on the input "not-a-file" the function should return 0 - print this result
+    
     Path filePath = Paths.get("not-a-file.txt");
     List<String> fileLines = readFile(filePath);
     List<char[]> charArrayList = getCharLists(fileLines);
--- seconds/Seconds.java
+++ seconds/Seconds.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 public class Seconds {
   public static void main(String[] args) {
-    // Create a function that takes a list as a parameter,
-    // and returns a new list with every second element from the orignal list
-    // example: [1, 2, 3, 4, 5] should produce [2, 4] - print this result
     List<Integer> myList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
     System.out.println(numbersList(myList));
   }
